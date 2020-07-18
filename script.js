f6eafe5140f5d036f109df3efdc84d88606616c1
Bernice2001 <wangechibanim@gmail.com>
2020-07-19 00:15:14 +0300

    body background repeat no repeat

--- script.js
+++ script.js
@@ -16,4 +16,30 @@ var femaleNames=[
     "Afua",
     "Ama",
 ];
-var 
+var weekDays=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+function additionSubmit() {
+    event.preventDefault();
+    var gender = document.getElementById('gender').value;
+    var date = document.getElementById('date').value;
+    date = new Date(date);
+
+    if (gender === 'female') {
+        alert(" SHE IS BORN  " + weekDays[date.getDay()] + ',' + " Your Akan Name Is " + femaleNames[date.getDay()]);
+        document.getElementById('result').innerHTML = femaleNames[date.getDay()]
+    }
+    else if (gender === 'male') {
+        alert(" HE IS BORN  " + weekDays[date.getDay()] + ',' + " Your Akan Name Is " + maleNames[date.getDay()]);
+        document.getElementById('result').innerHTML = maleNames[date.getDay()];
+    }
+    else {
+        alert( " IF SHE/HE IS BORN " + " UNDFINED DATE " + ',' + " Your Akan Name Is "  + " UNDEFINED ");
+    }
+}
